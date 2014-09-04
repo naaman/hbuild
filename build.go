d81276c164876a328466bd7b3d549cc90ebc9ffe
Naaman Newbold <naaman@heroku.com>
2014-09-03 18:35:15 -0700

    Build Status
    Adds the ability to obtain a build's status via the Heroku API. Status
    is stored in the BuildResponseJSON and can only be accessed using the
    Build.Status() method.

--- build.go
+++ build.go
@@ -6,9 +6,17 @@ import (
 )
 
 type Build struct {
-	Id              UUID   `json:"id"`
-	OutputStreamURL string `json:"output_stream_url"`
+	Id              UUID
 	Output          io.Reader
+	status					string
+	token						string
+	app         		string
+}
+
+type BuildResponseJSON struct {
+	Id              string `json:"id"`
+	OutputStreamURL string `json:"output_stream_url"`
+	Status					string `json:"status"`
 }
 
 type BuildRequestJSON struct {
@@ -19,24 +27,44 @@ type BuildRequestJSON struct {
 }
 
 func NewBuild(token, app string, source Source) (build Build, err error) {
-	buildJson := BuildRequestJSON{}
-	buildJson.SourceBlob.Url = source.Get.String()
+	buildReqJson := BuildRequestJSON{}
+	buildReqJson.SourceBlob.Url = source.Get.String()
 
 	client := newHerokuClient(token)
-	err = client.request(buildRequest(app, buildJson), &build)
+	buildResJson := BuildResponseJSON{}
+	err = client.request(buildRequest(app, buildReqJson), &buildResJson)
 	if err != nil {
 		return
 	}
 
-	stream, err := http.DefaultClient.Get(build.OutputStreamURL)
+	stream, err := http.DefaultClient.Get(buildResJson.OutputStreamURL)
 	if err != nil {
 		return
 	}
 
+	build.Id = UUID(buildResJson.Id)
+	build.token = token
+	build.app = app
 	build.Output = stream.Body
 	return
 }
 
+func (b *Build) Status() (string, error) {
+	buildJson := new(BuildResponseJSON)
+	client := newHerokuClient(b.token)
+
+	err := client.request(buildStatusRequest(*b), &buildJson)
+	if err != nil {
+		return "", err
+	}
+
+	return buildJson.Status, nil
+}
+
 func buildRequest(app string, build BuildRequestJSON) herokuRequest {
 	return herokuRequest{"POST", "/apps/" + app + "/builds", build}
+}
+
+func buildStatusRequest(build Build) herokuRequest {
+	return herokuRequest{"GET", "/apps/" + build.app + "/builds/" + string(build.Id), nil}
 }
