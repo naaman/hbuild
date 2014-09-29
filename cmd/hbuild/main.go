2fd41043c6eaf094243b8984b540fb2c08dc7518
Naaman Newbold <naaman@heroku.com>
2014-09-29 16:01:49 -0700

    Remove netrc parsing. All that biz can happen in an hk plugin.

--- cmd/hbuild/main.go
+++ cmd/hbuild/main.go
@@ -6,10 +6,8 @@ import (
 	"io"
 	"log"
 	"os"
-	"os/user"
 	"time"
 
-	"code.google.com/p/go-netrc/netrc"
 	"github.com/naaman/hbuild"
 )
 
@@ -17,14 +15,16 @@ var (
 	workDir, _ = os.Getwd()
 	fDir       = flag.String("source", workDir, "-source=/path/to/src")
 	fAppName   = flag.String("app", "", "-app=exampleapp")
-	fApiKey    = flag.String("key", netrcApiKey(), "-key=123ABC")
+	fApiKey    = flag.String("key", "", "-key=123ABC")
 )
 
 func init() {
 	flag.Parse()
-	if *fAppName == "" {
-		flag.Usage()
-		os.Exit(1)
+	for _, f := range []string{*fAppName, *fApiKey} {
+		if f == "" {
+			flag.Usage()
+			os.Exit(1)
+		}
 	}
 }
 
@@ -80,17 +80,4 @@ func main() {
 			return
 		}
 	}
-}
-
-func netrcApiKey() string {
-	if u, err := user.Current(); err == nil {
-		netrcPath := u.HomeDir + "/.netrc"
-		if _, err := os.Stat(netrcPath); err == nil {
-			key, _ := netrc.FindMachine(netrcPath, "api.heroku.com")
-			if key.Password != "" {
-				return key.Password
-			}
-		}
-	}
-	return ""
 }
